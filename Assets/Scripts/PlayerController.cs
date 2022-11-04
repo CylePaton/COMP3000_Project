bd90cc4b4344a0535e37607e4f2bc7520c343f16
Cyle Paton <cyle.paton@students.plymouth.ac.uk>
2022-11-04 23:34:10 +0000

    Added Melee Combat
    Player can now attack enemies using sword

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,15 +12,31 @@ public class PlayerController : MonoBehaviour
 
     //Values
     public float moveSpeed;
+    public float jumpForce;
+
+    public float constantGravity;
+    public float fallingGravity;
+    public float currentGravity;
+
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVecocity;
+
     private Vector3 moveDir;
+
     private bool isMoving;
     private bool isAttacking;
 
     //References
     private CharacterController controller;
     private Animator animator;
+    
+    public GameObject swordActive;
+    public GameObject swordSheathed;
+
+    public Transform attackArea;
+    public float attackRange = .5f;
+    public LayerMask enemyLayers;
+    public int attackDamage;
 
     private void Awake()
     {
@@ -51,13 +67,37 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         Attack();
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1"))
+        {
+            swordActive.SetActive(true);
+            swordSheathed.SetActive(false);
+        }
+        else
+        {
+            swordActive.SetActive(false);
+            swordSheathed.SetActive(true);
+        }
+
     }
+
+    private void Gravity()
+    {
+        if (controller.isGrounded)
+        {
+            currentGravity += constantGravity * Time.deltaTime;
+        }
+        else
+        {
+            currentGravity += fallingGravity * Time.deltaTime;
+        }
+    }   
 
     private void Movement()
     {
-        moveDir = new Vector3(move.ReadValue<Vector2>().x, 0f, move.ReadValue<Vector2>().y).normalized;
+        moveDir = new Vector3(move.ReadValue<Vector2>().x, currentGravity, move.ReadValue<Vector2>().y).normalized;
+        
         isMoving = moveDir.x != 0 || moveDir.z != 0;
-
         animator.SetBool("IsMoving", isMoving);
 
         Matrix4x4 matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
@@ -71,14 +111,29 @@ public class PlayerController : MonoBehaviour
 
             controller.Move(isoInput * moveSpeed * Time.deltaTime);
         }
-        
-
     }
 
     private void Attack()
     {
         playerInput.Player.Attack.started += ctx => isAttacking = ctx.ReadValueAsButton();
         playerInput.Player.Attack.canceled += ctx => isAttacking = ctx.ReadValueAsButton();
+        
         animator.SetBool("IsAttacking", isAttacking);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1"))
+        {
+            Collider[] hitEnemies =  Physics.OverlapSphere(attackArea.position, attackRange, enemyLayers);
+
+                    foreach(Collider enemy in hitEnemies)
+                    {
+                        enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+                    }
+        }
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackArea.position, attackRange);
     }
 }
